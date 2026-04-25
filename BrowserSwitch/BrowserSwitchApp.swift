ba79618eb5c62d229d9bc8aaf1904ae1d4699522
jeffrey200 <50335342+jeffrey200@users.noreply.github.com>
2026-04-25 11:32:05 +0200

    Initial Commit

---
+++ BrowserSwitch/BrowserSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserSwitchApp.swift
+//  BrowserSwitch
+//
+//  Created by Jeffrey on 25.04.26.
+//
+
+import SwiftUI
+
+@main
+struct BrowserSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
